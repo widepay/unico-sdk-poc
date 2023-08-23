c09b37f2e261594d96b93d3fb03c024b0878cf1a
Claudio Almeida <claudioalmeiida@gmail.com>
2023-08-23 07:40:20 -0400

    Update unico-sdk-poc.js

--- unico-sdk-poc.js
+++ unico-sdk-poc.js
@@ -4,11 +4,11 @@ let host = window.location.hostname;
 let config = null;
 
 if (host == 'dev.widepay.com') {
-    config = '{"project_info":{"project_number":"3777262724688960367547","project_id":"Wide Pay"},"client_info":{"mobilesdk_app_id":"3:394730:js","js_client_info":{"hostname":"dev.widepay.com"}},"host_info":{"host_info":"nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY","host_key":"kr+DXcacEEZugVGL9Sg1rKBpLWIbecEejxbAn3OPfyrtFa5CbLErt3yU0FNU5BQy"}}';
+    config = '{"project_info":{"project_number":"3777262724688960367547","project_id":"Wide Pay"},"client_info":{"mobilesdk_app_id":"3:394730:js","js_client_info":{"hostname":"https://dev.widepay.com"}},"host_info":{"host_info":"nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY","host_key":"kr+DXcacEEZugVGL9Sg1rKBpLWIbecEejxbAn3OPfyrtFa5CbLErt3yU0FNU5BQy"}}';
 } else if (host == 'www.widepay.com.br') {
-    config = '{"project_info":{"project_number":"699654794298494170","project_id":"Wide Pay"},"client_info":{"mobilesdk_app_id":"3:714159:js","js_client_info":{"hostname":"www.widepay.com.br"}},"host_info":{"host_info":"Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA=","host_key":"VzhxSn41Ol1mu7kx0mhqprOIUIQHrMWqxMauuXUYqft/vcjpS78GLjTzf3lw4/jH"}}';
+    config = '{"project_info":{"project_number":"699654794298494170","project_id":"Wide Pay"},"client_info":{"mobilesdk_app_id":"3:714159:js","js_client_info":{"hostname":"https://www.widepay.com.br"}},"host_info":{"host_info":"Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA=","host_key":"VzhxSn41Ol1mu7kx0mhqprOIUIQHrMWqxMauuXUYqft/vcjpS78GLjTzf3lw4/jH"}}';
 } else {
-    config = '{"project_info":{"project_number":"220205842671153458","project_id":"Wide Pay"},"client_info":{"mobilesdk_app_id":"3:394730:js","js_client_info":{"hostname":"www.widepay.com"}},"host_info":{"host_info":"Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA=","host_key":"Xgq1C7ba3139jhHxwOMcXU1U6hFbTmDEvQO5pPoZ8J5MgIaDAAEtTtUO7afSDLfv"}}';
+    config = '{"project_info":{"project_number":"220205842671153458","project_id":"Wide Pay"},"client_info":{"mobilesdk_app_id":"3:394730:js","js_client_info":{"hostname":"https://www.widepay.com"}},"host_info":{"host_info":"Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA=","host_key":"Xgq1C7ba3139jhHxwOMcXU1U6hFbTmDEvQO5pPoZ8J5MgIaDAAEtTtUO7afSDLfv"}}';
 }
 
 const debug = document.getElementById('debug');
